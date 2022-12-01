c12532f2b5b30174f45257cc05a817f0f8cda337
shubhamsable2000 <sable.s@northeastern.edu>
2022-12-01 13:03:44 -0500

    Network

---
+++ src/Business/Network/Network.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.Network;
+
+import Business.Enterprise.EnterpriseDirectory;
+
+/**
+ *
+ * @author shubhamsable
+ */
+public class Network {
+    private String name;
+    private String state;
+    private String country;
+    
+    
+    
+    private EnterpriseDirectory enterpriseDirectory;
+    
+    public Network(){
+        enterpriseDirectory=new EnterpriseDirectory();
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public EnterpriseDirectory getEnterpriseDirectory() {
+        return enterpriseDirectory;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+    
+    
+    
+    
+    @Override
+    public String toString(){
+        return name;
+    }
+    
+    
+}
